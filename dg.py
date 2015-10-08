853924a4e0fe22b401bf0092fbedc292d0ba5127
wtgme <tw5n14@UOS-211330.soton.ac.uk>
2015-10-08 16:21:10 +0100

    Finish analysis of directed graph

--- dg.py
+++ dg.py
@@ -25,14 +25,13 @@ with open('mrredges-no-tweet-no-retweet-poi-counted.txt', 'r') as fo:
         b_type = tokens[2]
         weightv = int(tokens[3])
         # reply-to mentioned
-        if b_type == 'reply-to':
+        if b_type == 'mentioned':
             DG.add_edge(n1, n2, weight=weightv)
             # print n1, n2, weightv
 
         
 # pos = nx.random_layout(DG)
 # pos = nx.shell_layout(DG)
-
 #pos = nx.spring_layout(DG)
 #pos = nx.spectral_layout(DG)
 #nx.draw(DG, pos)
@@ -77,63 +76,75 @@ print 'The number of self-loop: %d' %(DG.number_of_selfloops())
 
     
 
-##plot the numbers of nodes with degree K
-#plt.title('Numbers of Nodes with Degree K Plot(reply)')
-#plt.ylabel('Number of nodes')
+#plot the numbers of nodes with degree K
+plt.title('Numbers of Nodes with In-degree K Plot(mention)')
+plt.ylabel('Number of nodes')
+plt.xlabel('Degree')
+degseq=list(DG.in_degree(weight='weight').values())
+print degseq
+dmax=max(degseq)+1
+freq= [ 0 for d in range(dmax) ]
+for d in degseq:
+    freq[d] += 1
+plt.plot(freq)
+#plt.plot(list(nx.utils.cumulative_sum(freq)))
+plt.show()
+
+##plot cumulative distribution of degree K
+#plt.title('Cumulative Distribution of Nodes with Degree K Plot(mention)')
+#plt.ylabel('P')
+#plt.ylim(0.0,1.1)
 #plt.xlabel('Degree')
 #degseq=list(DG.degree(weight='weight').values())
 #dmax=max(degseq)+1
 #freq= [ 0 for d in range(dmax) ]
 #for d in degseq:
 #    freq[d] += 1
-#plt.plot(freq)
-##plt.plot(list(nx.utils.cumulative_sum(freq)))
+##plt.plot(freq)
+#sumall = sum(freq)
+#cumul = []
+#temp = 0.0
+#for fre in freq:
+#    temp += fre
+#    cumul.append(temp/sumall)
+#plt.plot(cumul)
 #plt.show()
 
-#plot cumulative distribution of degree K
-plt.title('Cumulative Distribution of Nodes with Degree K Plot(reply)')
-plt.ylabel('P')
-plt.ylim(0.0,1.0)
-plt.xlabel('Degree')
-degseq=list(DG.degree(weight='weight').values())
-dmax=max(degseq)+1
-freq= [ 0 for d in range(dmax) ]
-for d in degseq:
-    freq[d] += 1
-#plt.plot(freq)
-plt.plot(list(float(nx.utils.cumulative_sum(freq))/sum(freq)))
-plt.show()
-
-
+##histogram of path lengths
 #print 'source vertex {taget: length,}'
 #pathlengths = []
 #for v in DG.nodes():
 #    spl = single_source_shortest_path_length(DG, v)
-#    print '%s %s' %(v, spl)
+##    print '%s %s' %(v, spl)
 #    for p in spl.values():
 #        pathlengths.append(p)
 #print 'average shortest path length %s' %(sum(pathlengths)/len(pathlengths))
-
-
-##histogram of path lengths
+#
 #dist = {}
 #for p in pathlengths:
 #    v = dist.get(p,0)+1
 #    dist[p] = v
 #print 'length #paths'
 #verts = dist.keys()
+#distlist = []
 #for d in sorted(verts):
+#    distlist.append(dist[d])
 #    print '%s %d' %(d, dist[d])
-#
+#plt.title('Plot of Shortest Path and Numbers of Paths(reply)')
+#plt.ylabel('Counts')
+#plt.xlabel('Path Length')
+##plt.ylim(ymin=-10.0)
+#plt.plot(distlist)
+
 #print (is_connected(DG))
-#
+
 #print 'radius: %d' %(radius(DG))
 #print 'diameter: %d' %(diameter(DG))
 #print 'eccentricity: %s' %(eccentricity(DG))
 #print 'center: %s' %(center(DG))
 #print 'periphery: %s' %(periphery(DG))
 #print 'density: %s' %(density(DG))
-#
+
 #nx.k_clique_communities(DG, 5)
 #nx.draw(DG)
 #plt.show()
