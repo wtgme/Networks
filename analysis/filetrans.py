991ac9fcbacf501b0394308881b01335c718f715
wtgme <wtgmme@gmail.com>
2015-11-04 13:09:11 +0000

    change file dir

---
+++ analysis/filetrans.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 17:48:15 2015
+
+@author: wt
+"""
+
+import csv
+
+userIdMap = {}
+
+with open('mrredges-no-tweet-no-retweet-poi-counted.csv', 'rt') as f:
+    reader = csv.reader(f)
+    first_row = next(reader)
+    for row in reader:
+        u1 = row[0]
+        u2 = row[1]
+        btype = row[2]
+        count = row[3]
+        userID = userIdMap.get(u1,len(userIdMap))
+        userIdMap[u1] = userID
+        userID = userIdMap.get(u2,len(userIdMap))
+        userIdMap[u2] = userID
+        print u1, u2, btype, count
+
+
+
